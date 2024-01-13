26d7f982ae765e990a42f9a17467a6b38993103c
matheus-gomesf <matheusg.code@gmail.com>
2024-01-13 00:09:55 -0300

    Initial commit

---
+++ src/main/java/com/br/mgf/desafioblog/DesafioBlogApplication.java
@@ -0,0 +1,13 @@
+package com.br.mgf.desafioblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/mgf/desafioblog/DesafioBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.mgf.desafioblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
