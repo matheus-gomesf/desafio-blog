4ab84e4e4d2b2e9b67f6eac3b11499110c385af1
matheus-gomesf <matheusg.code@gmail.com>
2024-01-13 01:52:44 -0300

    Create facede to get logged-in user.

---
+++ src/main/java/com/br/mgf/desafioblog/security/IAuthenticationFacade.java
@@ -0,0 +1,7 @@
+package com.br.mgf.desafioblog.security;
+
+import org.springframework.security.core.Authentication;
+
+public interface IAuthenticationFacade {
+    Authentication getAuthentication();
+}
